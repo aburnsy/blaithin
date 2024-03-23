64625cd8530e610428677964c7206854d9e0934a
Andy Burns <andrewburns89@gmail.com>
2024-03-23 21:59:27 +0000

    Add to gardens4you

--- bronze/tullys.py
+++ bronze/tullys.py
@@ -10,47 +10,42 @@ def parse_products(URL: str, category: str, products: list) -> list[dict]:
     results = []
     # Skip the first row as that as the heading though its not labeled as such
     for product in products[1:]:
+        product_link = product.find("a", class_="productLink")
+        product_url = urljoin(URL, product_link["href"])
+        product_name = product.find("p", class_="productName").text
+        product_code = product.find("p", class_="productCode").find("span").text
+        image = product.find("div", class_="imageWrap").find("img")
+        if image is None:
+            img_url = None
+        else:
+            img_url = urljoin(URL, image["src"]).rsplit("&Thumbnail=true", 1)[0]
+        description = product.find("p", class_="description").find("span").text
+        price_exc_vat = (
+            product.find("div", class_="priceVatExcCurrencySymbol")
+            .find_all("span")[1]
+            .text
+        )
+        price_inc_vat = round(float(price_exc_vat) * 1.135, 2)
+        size = product.find("div", class_="columnField").find("span").text
         try:
-            product_link = product.find("a", class_="productLink")
-            product_url = urljoin(URL, product_link["href"])
-            product_name = product.find("p", class_="productName").text
-            product_code = product.find("p", class_="productCode").find("span").text
-            image = product.find("div", class_="imageWrap").find("img")
-            if image is None:
-                img_url = None
-            else:
-                img_url = urljoin(URL, image["src"]).rsplit("&Thumbnail=true", 1)[0]
-            description = product.find("p", class_="description").find("span").text
-            price_exc_vat = (
-                product.find("div", class_="priceVatExcCurrencySymbol")
-                .find_all("span")[1]
-                .text
-            )
-            price_inc_vat = round(float(price_exc_vat) * 1.135, 2)
-            size = product.find("div", class_="columnField").find("span").text
-            try:
-                stock = int(product.find("div", class_="inStock").text)
-            except ValueError:
-                stock = 0
-            results.append(
-                {
-                    "source": "tullys",
-                    "source_url": URL,
-                    "product_url": product_url,
-                    "category": category,
-                    "product_name": product_name,
-                    "product_code": product_code,
-                    "img_url": img_url,
-                    "description": description,
-                    "price": price_inc_vat,
-                    "size": size,
-                    "stock": stock,
-                }
-            )
-        except Exception as error:
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            message = template.format(type(error).__name__, error.args)
-            print(message)
+            stock = int(product.find("div", class_="inStock").text)
+        except ValueError:
+            stock = 0
+        results.append(
+            {
+                "source": "tullys",
+                "source_url": URL,
+                "product_url": product_url,
+                "category": category,
+                "product_name": product_name,
+                "product_code": product_code,
+                "img_url": img_url,
+                "description": description,
+                "price": price_inc_vat,
+                "size": size,
+                "stock": stock,
+            }
+        )
     return results
 
 
--- load_bronze_data.py
+++ load_bronze_data.py
@@ -1,13 +1,14 @@
 import argparse
 import bronze.tullys as tullys
 import bronze.quickcrop as quickcrop
+import bronze.gardens4you as gardens4you
 import cloud_storage
 
 
 def main(params):
     root_path = f"{params.bucket_name}/bronze"
 
-    match params.sites:
+    match params.site:
         case "tullys":
             cloud_storage.export_data_locally(
                 table=tullys.get_product_data("tullys_test"), root_path=root_path
@@ -17,12 +18,20 @@ def main(params):
                 table=quickcrop.get_product_data("quickcrop_test"),
                 root_path=root_path,
             )
+        case "gardens4you":
+            cloud_storage.export_data_locally(
+                table=gardens4you.get_product_data("gardens4you_test"),
+                root_path=root_path,
+            )
         case _:
             cloud_storage.export_data_to_gcs(
                 table=tullys.get_product_data(), root_path=root_path
             )
             cloud_storage.export_data_to_gcs(
                 table=quickcrop.get_product_data(), root_path=root_path
+            )
+            cloud_storage.export_data_to_gcs(
+                table=gardens4you.get_product_data(), root_path=root_path
             )
 
 
@@ -34,9 +43,9 @@ if __name__ == "__main__":
         "--bucket_name", help="Bucket name in GCS to store data to.", required=True
     )
     parser.add_argument(
-        "--sites",
+        "--site",
         help="Name of the site you would like to fetch data for.",
-        choices=["tullys", "quickcrop"],
+        choices=["tullys", "quickcrop", "gardens4you"],
     )
     args = parser.parse_args()
     main(args)
