2c1ed602513ed9158f60449eb0fc0209e5a827a3
Andy Burns <andrewburns89@gmail.com>
2024-03-28 17:34:44 +0000

    Export to gcs daily

--- docker.sh
+++ docker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker 
+
+# Fetch Terraform output for use within Mage
+export PROJECT_NAME=blaithin
+export GCS_BUCKET_NAME=$(terraform -chdir=Terraform output -raw bucket_name)
+
+cd mage && docker compose up -d
