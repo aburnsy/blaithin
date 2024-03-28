c7f0082694fde57deeff508960812ac4414398f1
Andy Burns <andrewburns89@gmail.com>
2024-03-28 10:27:14 +0000

    Fetch data from github

--- mage/blaithin/data_loaders/load_from_github.py
+++ mage/blaithin/data_loaders/load_from_github.py
@@ -3,10 +3,12 @@ if 'data_loader' not in globals():
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 import polars as pl
-from urllib.parse import urlparse
 import os
+import shutil
+from dateutil import rrule
+from datetime import datetime
 
-BASE_URL = 'https://github.com/aburnsy/blaithin_files/tree/main/data'
+BASE_URL = 'https://github.com/aburnsy/blaithin_files/raw/main/data'
 
 @data_loader
 def load_data(*args, **kwargs):
@@ -16,20 +18,35 @@ def load_data(*args, **kwargs):
     Returns:
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
-    # date_format = kwargs['plant_type']
-    site = 'arboretum' 
-    # //kwargs['site'] or 
-    file_date = "2024-03-27"
+    site = kwargs['site']
 
-    parquet_url = f'https://github.com/aburnsy/blaithin_files/blob/main/data/{site}/{file_date}.parquet'
-    base_file = f"/files/{site}/{file_date}.parquet"
-    os.system(f"curl -L0 {parquet_url} --Output /{file_date}{site}.parquet")
-    path = urlparse(parquet_url).path
-    parquet_file_path = f"./{os.path.basename(path)}"
-    print(f"{path}")
-    print(parquet_file_path)
-    df = pl.read_parquet(base_file)
+    try:
+        os.mkdir(site)
+    except FileExistsError:
+        shutil.rmtree(site)
+        os.mkdir(site)
+
+    try:
+        kwargs['load_all_flag']
+        file_dates = [dt.strftime('%Y-%m-%d') for dt in rrule.rrule(rrule.DAILY, dtstart=datetime(2024, 3, 26), until=datetime.now())]
+    except KeyError:
+        file_dates = [kwargs['execution_date'].strftime('%Y-%m-%d')]
+
+    print(f'loading files for dates [{file_dates}] from site: {site}')
+
+    for file_date in file_dates:
+        parquet_url = f'{BASE_URL}/{site}/{file_date}.parquet'
+        base_file = f"{site}/{file_date}.parquet"
+        if not os.system(f"curl -sfLo/dev/null -r0-0 {parquet_url}"):
+            os.system(f"curl -L {parquet_url} --Output {base_file}")
+        else:
+            print(f"File does not exist at {parquet_url}")
+    
+    df = pl.read_parquet(f"{site}/*")
+
+    shutil.rmtree(site)
     return df
+
 
 
 @test
